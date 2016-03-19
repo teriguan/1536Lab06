388e3727abff892320a3d8ce5d875f884917c679
elichu <elichu0@gmail.com>
2016-03-19 10:03:45 -0700

    thankyou.html works for all values in users.txt

--- registered.php
+++ registered.php
@@ -5,16 +5,16 @@
 	$email = strtolower($_POST["email"]);
 	$val = array($first, $last, $email);
 	$str = implode(" ", $val);
-	
-	//$file = @fopen('users.txt', 'r');
-	
-	//$file = 'users.txt';
+
 	$contents = file_get_contents('users.txt');
 	$confirm = strpos($contents, $str);
 	if($confirm) {
 	header("Location:./thankyou.html");
 	exit();
-	} else {
+	} else if($confirm === 0) {
+	header("Location:./thankyou.html");
+	exit(); 
+	} else {	
 	header("Location:./sorry.html");
 	exit();
 	}
